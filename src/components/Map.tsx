1e32f9203d6e12fe1c8a6588912d5d846b2cae84
timf34 <farrelti@tcd.ie>
2024-08-04 03:12:34 +0100

    added a nicer/ smoother colour to the map markers

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -113,7 +113,9 @@ export default function Map() {
                 </div>`
             );
 
-            new mapboxgl.Marker()
+            new mapboxgl.Marker({
+                color: '#74b6c4',
+            })
                 .setLngLat([location.longitude, location.latitude])
                 .setPopup(popup)
                 .addTo(map.current!);
