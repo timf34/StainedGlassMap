088f58107a2695c59f065cf341d93d4e4b6ea952
timf34 <farrelti@tcd.ie>
2024-08-04 13:26:01 +0100

    removed unused type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,17 +1,3 @@
-export interface StainedGlassLocation {
-    id: string;
-    name: string;
-    artist: string;
-    county: string;
-    latitude: number;
-    longitude: number;
-    year?: number;
-    description?: string;
-    thumbnail_url: string;
-    additional_images?: string[];
-    google_maps_url: string;
-}
-
 export interface LocationWithDetails {
     id: number;
     name: string;
