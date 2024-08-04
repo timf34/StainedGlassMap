583e584ed9435ea0fa5370b59300ea0148c8e34b
timf34 <farrelti@tcd.ie>
2024-08-04 02:42:14 +0100

    fixed layout - now the map fits onto the desktop without scrolling, and scrolling is reduced to the LocationList rather than including the filter boxes too

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,6 @@ import FilterBox from '../components/FilterBox';
 import LocationList from "../components/LocationList";
 
 export default function Home() {
-    const artistOptions = ['Artist 1', 'Artist 2', 'Artist 3']; // Replace with your actual options
-    const countyOptions = ['County 1', 'County 2', 'County 3']; // Replace with your actual options
 
     const theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
@@ -18,11 +16,15 @@ export default function Home() {
             <header className="w-full p-4 bg-white">
                 <h1 className="text-2xl md:text-4xl font-light font-montserrat italic">Stained Glass Map of Ireland</h1>
             </header>
-            <div className={`flex flex-grow ${isMobile ? 'flex-col' : 'flex-row'}`}>
-                <aside className={`${isMobile ? 'w-full' : 'w-1/5'} p-4 bg-gray-50 overflow-y-auto`}>
-                    <FilterBox tableName="artists" placeholder="Filter by Artist"/>
-                    <FilterBox tableName="counties" placeholder="Filter by County"/>
-                    <LocationList/>
+            <div className={`flex flex-grow ${isMobile ? 'flex-col' : 'flex-row'} overflow-hidden`}>
+                <aside className={`${isMobile ? 'w-full' : 'w-1/5'} p-4 bg-gray-50 flex flex-col`}>
+                    <div className="mb-4">
+                        <FilterBox tableName="artists" placeholder="Filter by Artist"/>
+                        <FilterBox tableName="counties" placeholder="Filter by County"/>
+                    </div>
+                    <div className="flex-grow overflow-y-auto">
+                        <LocationList/>
+                    </div>
                 </aside>
                 <section className={`${isMobile ? 'w-full h-[50vh]' : 'w-4/5'} p-4`}>
                     <Map/>
